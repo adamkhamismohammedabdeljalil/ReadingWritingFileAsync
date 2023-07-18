29c56e28086dcf43f075e86b3464acbc0115f623
adamkhamismohammedabdeljali <adamkhamismohammedabdeljalil@gmail.com>
2023-07-18 11:55:48 +0200

    feat:add project ReadingWritingAsynchronously

---
+++ ReadingWritingAsynchronously/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await ReadFromFileAsync();
+    }
+
+    static async Task ReadFromFileAsync()
+    {
+        Console.WriteLine("Please write some text::");
+        string userInput = Console.ReadLine();
+
+        await File.WriteAllTextAsync("file.txt", userInput);
+        Console.WriteLine("Content written to the file successfully.");
+
+        string fileContent = await File.ReadAllTextAsync("file.txt");
+        Console.WriteLine($"File read successfully: \n{fileContent}");
+    }
+}
